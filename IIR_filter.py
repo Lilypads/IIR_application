edbf1471f7cdcfed09aa3efb346fe133ceaa22ec
Lilypads <67157765+Lilypads@users.noreply.github.com>
2020-11-26 17:31:39 +0000

    update

--- IIR_filter.py
+++ IIR_filter.py
@@ -13,7 +13,7 @@ class IIR_filter_2ndOrder:
         
     def dofilter(self,x):
         self.acc = (x*self.b0) + (self.b_x1*self.b1) + (self.b_x2*self.b2) - (self.b_y1*self.a1) - (self.b_y2*self.a2)
-        self.b_x2 = self.b_x1      # delay lines
+        self.b_x2 = self.b_x1      # update delay lines
         self.b_x1 = x
         self.b_y2 = self.b_y1
         self.b_y1 = self.acc
