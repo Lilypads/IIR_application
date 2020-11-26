21349a617320383dd419722bbcb3a4060336ba21
Lilypads <67157765+Lilypads@users.noreply.github.com>
2020-11-26 18:03:05 +0000

    update

--- realtime_iir_main.py
+++ realtime_iir_main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from IIR_filter import IIR_filter_2ndOrder
-from pynput.keyboard import Key, Controller # Adds controller to keyboard
+from pynput.keyboard import Key, Controller # Add keyboard controller
 
 # Realtime oscilloscope at a sampling rate of 50Hz
 # It displays analog channel 0.
@@ -59,14 +59,16 @@ realtimePlotWindow2 = RealtimePlotWindow()
 # sampling rate: 100Hz
 samplingRate = 100
 
-filterDC = IIR_filter_2ndOrder( 0.78429785, -1.56859571,  0.78429785, -1.76995414, 0.78402168)
-filterDC2 = IIR_filter_2ndOrder(1.        , -2.        ,  1.        , -1.82269493, 0.83718165)
-filterDC3 = IIR_filter_2ndOrder( 1.        , -2.        ,  1.        , -1.92188606, 0.93716115)
+# instantiate the iir filter with coefficients
+filterDC = IIR_filter_2ndOrder(0.78429785, -1.56859571, 0.78429785, -1.76995414, 0.78402168)
+filterDC2 = IIR_filter_2ndOrder(1.        , -2.        , 1.        , -1.82269493, 0.83718165)
+filterDC3 = IIR_filter_2ndOrder(1.        , -2.        , 1.        , -1.92188606, 0.93716115)
 
-filterLP = IIR_filter_2ndOrder(1.11171764e-02, -1.77905018e-04,  1.11171764e-02, -1.23046251e+00,  3.91758991e-01)
-filterLP2 = IIR_filter_2ndOrder(1.00000000e+00, -1.53404451e+00,  1.00000000e+00, -1.48743241e+00,  6.23734470e-01)
-filterLP3 = IIR_filter_2ndOrder(1.00000000e+00, -1.73602396e+00,  1.00000000e+00, -1.75191261e+00,  8.75313200e-01)
+filterLP = IIR_filter_2ndOrder(1.11171764e-02, -1.77905018e-04, 1.11171764e-02, -1.23046251e+00, 3.91758991e-01)
+filterLP2 = IIR_filter_2ndOrder(1.00000000e+00, -1.53404451e+00, 1.00000000e+00, -1.48743241e+00, 6.23734470e-01)
+filterLP3 = IIR_filter_2ndOrder(1.00000000e+00, -1.73602396e+00, 1.00000000e+00, -1.75191261e+00, 8.75313200e-01)
 
+# instantiate keyboard controller
 keyboard = Controller()
 
 # called for every new sample which has arrived from the Arduino
@@ -74,20 +76,21 @@ def callBack(data):
     
     # send the sample to the plotwindbow
     # add any filtering here:
+        # data = self.myfilter.dofilter(data)
         
-    intermediate  = filterDC.dofilter(data)
+    intermediate = filterDC.dofilter(data)
     intermediate = filterDC2.dofilter(intermediate)
     output = filterDC3.dofilter(intermediate)
     
-    intermediate  = filterLP.dofilter(output)
-    intermediate  = filterLP2.dofilter(intermediate)
-    output        = filterLP3.dofilter(intermediate)
-    # data = self.myfilter.dofilter(data)
+    intermediate  = filterLP.dofilter(output)           # comment these 3 out for DC filter only
+    intermediate  = filterLP2.dofilter(intermediate)    # comment these 3 out for DC filter only
+    output        = filterLP3.dofilter(intermediate)    # comment these 3 out for DC filter only
     
+    # jump
     if output >= 0.04:
         keyboard.press(Key.space)
         keyboard.release(Key.space)
-        print("SpaceJump")
+        #print("SpaceJump")
         
     realtimePlotWindow.addData(data)
     realtimePlotWindow2.addData(output)
@@ -100,7 +103,7 @@ board = Arduino('COM3')
 board.samplingOn(1000 / samplingRate)
 
 # Register the callback which adds the data to the animated plot
-board.analog[3].register_callback(callBack)
+board.analog[3].register_callback(callBack)         # analog pin a3 of Arduino connects to z-axis of the sensor
 # Enable the callback
 board.analog[3].enable_reporting()
 
