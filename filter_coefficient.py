d3d0651e153c00970b77529ccaa57d338931dab3
Lilypads <67157765+Lilypads@users.noreply.github.com>
2020-12-03 18:31:19 +0000

    update

--- filter_coefficient.py
+++ filter_coefficient.py
@@ -12,7 +12,9 @@ sos = sig.butter(6,fc/fs*2,btype='high',output='sos')
 w,h = sig.freqz(b,a)
 pyplot.figure(1)
 pyplot.plot(w/np.pi/2,20*np.log10(np.abs(h)))
-
+pyplot.title('Frequency Response DC')
+pyplot.xlabel('Normalised Frequency')
+pyplot.ylabel('Amplitude')
 
 # deleting high frequency noise
 fc = 8      # cutoff frequency
@@ -22,3 +24,6 @@ sos = sig.cheby2(6,40,fc/fs*2,btype='low',output='sos')
 w,h = sig.freqz(b,a)
 pyplot.figure(2)
 pyplot.plot(w/np.pi/2,20*np.log10(np.abs(h)))
+pyplot.title('Frequency Response High Frequency Noise')
+pyplot.xlabel('Normalised Frequency')
+pyplot.ylabel('Amplitude')
